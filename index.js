933045ce507da62276327959b492646d8409e00f
ist188123 <paulorosario@sapo.pt>
2019-05-02 09:23:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,126 @@
+"use strict";
+
+const log = require('loglevel');
+require('loglevel-prefix-persist/server')(process.env.NODE_ENV, log, {
+	level: {
+		production: 'debug',
+		development: 'debug'
+	},
+	persist: 'debug',
+	max: 5
+});
+
+log.setLevel('debug');
+
+const private_settings = require('./data/private-settings'),
+	Commando = require('discord.js-commando'),
+	Client = new Commando.Client({
+		owner: private_settings.owner,
+		restWsBridgeTimeout: 10000,
+		restTimeOffset: 1000
+	}),
+	DB = require('./app/db.js'),
+	NodeCleanup = require('node-cleanup'),
+	Helper = require('./app/helper'),
+	IP = require('./app/process-image'),
+	Raid = require('./app/raid'),
+	Utility = require('./app/utility');
+
+NodeCleanup((exitCode, signal) => {
+	Raid.shutdown();
+});
+
+Client.registry.registerGroup('admin', 'Administration');
+Client.registry.registerGroup('basic-raid', 'Raid Basics');
+Client.registry.registerGroup('raid-crud', 'Raid Creation and Maintenance');
+Client.registry.registerGroup('roles', 'Roles');
+
+Client.registry.registerDefaultTypes();
+Client.registry.registerDefaultGroups();
+
+Client.registry.registerCommand(require('./commands/util/help'));
+
+Client.registry.registerDefaultCommands({help: false});
+
+Client.registry.registerTypesIn(__dirname + '/types');
+
+Client.registry.registerCommands([
+	require('./commands/admin/asar'),
+	require('./commands/admin/rsar'),
+	require('./commands/admin/lsar'),
+
+	require('./commands/raids/join'),
+	require('./commands/raids/interested'),
+	require('./commands/raids/check-in'),
+	require('./commands/raids/done'),
+
+	require('./commands/raids/check-out'),
+	require('./commands/raids/leave'),
+
+	require('./commands/raids/start-time'),
+	require('./commands/raids/status'),
+	require('./commands/raids/directions'),
+
+	require('./commands/raids/create'),
+	require('./commands/raids/delete'),
+
+	require('./commands/raids/hatch-time'),
+	require('./commands/raids/time-left'),
+	require('./commands/raids/set-pokemon'),
+	require('./commands/raids/set-location'),
+
+	require('./commands/roles/iam'),
+	require('./commands/roles/iamnot')
+]);
+
+let is_initialized = false;
+
+Client.on('ready', () => {
+	log.info('Client logged in');
+
+	// Only initialize various classes once ever since ready event gets fired
+	// upon reconnecting after longer outages
+	if (!is_initialized) {
+		Helper.setClient(Client);
+		Raid.setClient(Client);
+		DB.initialize(Client.guilds);
+		IP.initialize();
+
+		is_initialized = true;
+	}
+});
+
+Client.on('error', err => log.error(err));
+Client.on('warn', err => log.warn(err));
+Client.on('debug', err => log.debug(err));
+
+Client.on('rateLimit', event =>
+	log.warn(`Rate limited for ${event.timeout} ms, triggered by method '${event.method}', path '${event.path}', route '${event.route}'`));
+
+Client.on('commandRun', (command, result, message, args, from_pattern) => {
+	log.debug(`Command '${command.name}' run from message '${message.content}' by user ${message.author.id}`);
+	message.is_successful = true;
+});
+
+Client.on('commandError', (command, err, message, args, from_pattern) => {
+	log.error(`Command '${command.name}' error from message '${message.content}' by user ${message.author.id}`);
+});
+
+Client.on('commandFinalize', (command, message, from_pattern) => {
+	Utility.cleanConversation(message, !!message.is_successful, !Raid.validRaid(message.channel.id));
+});
+
+Client.on('disconnect', event => {
+	log.error(`Client disconnected, code ${event.code}, reason '${event.reason}'...`);
+
+	Client.destroy()
+		.then(() => Client.login(private_settings.discord_bot_token));
+});
+
+Client.on('reconnecting', () => log.info('Client reconnecting...'));
+
+Client.on('guildUnavailable', guild => {
+	log.warn(`Guild ${guild.id} unavailable!`);
+});
+
+Client.login(private_settings.discord_bot_token);
---
+++ types/pokemon.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const Commando = require('discord.js-commando'),
+	Pokemon = require('../app/pokemon');
+
+class PokemonType extends Commando.ArgumentType {
+	constructor(client) {
+		super(client, 'pokemon');
+	}
+
+	validate(value, message, arg) {
+		const terms = value.split(/[\s-]/)
+				.map(term => term.toLowerCase()),
+			pokemon = Pokemon.search(terms);
+
+		if (!pokemon) {
+			return 'No pokémon found.  Please try your search again, entering the text you want to search for.\n';
+		}
+
+		if (!pokemon.exclusive && !pokemon.tier) {
+			const name = pokemon.name ?
+				`"${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}"` :
+				'Pokémon';
+
+			return `${name} is not a valid raid boss.  Please try your search again, entering the text you want to search for.\n`;
+		}
+
+		return true;
+	}
+
+	parse(value, message, arg) {
+		const terms = value.split(/[\s-]/)
+			.map(term => term.toLowerCase());
+
+		return Pokemon.search(terms);
+	}
+}
+
+module.exports = PokemonType;
